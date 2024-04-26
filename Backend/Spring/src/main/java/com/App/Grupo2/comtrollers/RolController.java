d71663951d9644e6e3c7141f9371a33083ba033c
Ivan Andrada <ivanandradavalenzuela@gmail.com>
2024-04-26 14:13:17 +0200

    Actualizacion service Rol

--- Backend/Spring/src/main/java/com/App/Grupo2/comtrollers/RolController.java
+++ Backend/Spring/src/main/java/com/App/Grupo2/comtrollers/RolController.java
@@ -1,14 +1,15 @@
 package com.App.Grupo2.comtrollers;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.App.Grupo2.models.Rol;
-import com.App.Grupo2.repositories.RolRepository;
+
+import com.App.Grupo2.services.RolServices;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,47 +19,42 @@ import org.springframework.web.bind.annotation.PutMapping;
 @CrossOrigin(origins = "*")
 @RestController
 public class RolController {
-    RolRepository r;
 
-    RolController(RolRepository r) {
-        this.r = r;
+    RolServices rolServices;
+
+    RolController(RolServices rolServices) {
+        this.rolServices = rolServices;
     }
 
     @GetMapping("/roles")
     public List<Rol> getAll() {
-        return this.r.findAll();
+        return rolServices.getAll();
     }
 
     @GetMapping("/roles/id/{id}")
     public Rol oneById(@PathVariable("id") int id) {
-        Optional<Rol> lr = this.r.findById(id);
-        if (lr.isEmpty()) {
-            return null;
-        }
-        return lr.get();
+        return rolServices.oneById(id).orElse(null);
     }
 
     @PostMapping("/roles")
     public int addRol(@RequestBody Rol entity) {
-        Rol rol = this.r.saveAndFlush(entity);
+        Rol rol = rolServices.addRol(entity);
         return rol.getId();
     }
 
     @DeleteMapping("/roles/{id}")
     public String deleteRol(@PathVariable("id") int id) {
-        this.r.deleteById(id);
+        rolServices.deleteRol(id);
         return "Rol eliminado";
     }
 
     @PutMapping("/roles/{id}")
-    public String putRol(@PathVariable int id, @RequestBody Rol entity) {
-        Rol rolTemp = this.oneById(id);
-        if (rolTemp == null) {
-            return "No encontrado";
+    public String putRol(@PathVariable int id, @RequestBody Rol rolToUpdate) {
+        Rol rol = rolServices.putRol(id, rolToUpdate);
+        if (rol != null) {
+            return "Rol actualizado";
         }
-        rolTemp.setName(entity.getName());
-        this.r.save(entity);
-        return "Rol actualizado";
+        return "Rol no encontrado";
     }
 
 }
