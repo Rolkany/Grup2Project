d6e74c0854c8c3b58cc4148b5f42f43ac3315fab
Rolkany <rolkanys@gmail.com>
2024-05-04 19:41:11 +0200

    Ordening and comment code

--- Backend/Spring/src/main/java/com/App/Grupo2/models/Location.java
+++ Backend/Spring/src/main/java/com/App/Grupo2/models/Location.java
@@ -1,11 +1,14 @@
+/* ---------- Package containing repository classes ---------- */
 package com.App.Grupo2.models;
 
+/* ---------- Imports ---------- */
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
+/* ----- Spring annotations ----- */
 @Data
 @Entity
 public class Location {
