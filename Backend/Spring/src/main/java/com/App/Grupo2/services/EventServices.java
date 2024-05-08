3949a302c53078206dce0b590dc0540301feddc8
davidubi <eros_eros_30@hotmail.com>
2024-05-08 14:27:02 +0200

    David

---
+++ Backend/Spring/src/main/java/com/App/Grupo2/services/EventServices.java
@@ -0,0 +1,52 @@
+package com.App.Grupo2.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.App.Grupo2.models.Event;
+
+import com.App.Grupo2.repositories.EventRepository;
+
+@Service
+public class EventServices {
+    private EventRepository eventRepository;
+    
+    EventServices(EventRepository eventRepository){
+        this.eventRepository=eventRepository;
+
+    }
+
+    public List<Event> getAll(){
+
+        return eventRepository.findAll();
+
+        }
+
+    public Optional<Event> oneById(int id){
+     
+        return eventRepository.findById(id);
+
+    }
+
+    public Event addEvent(Event newEvent){
+    
+        return eventRepository.saveAndFlush(newEvent);
+}
+    
+      public String deleteEventById(int id){
+
+        if(eventRepository.existsById(id)){
+            eventRepository.deleteById(id);
+            return "Evento eliminado";
+        }
+
+        return "Evento no encontrado";
+
+      }
+
+      
+
+    
+}
