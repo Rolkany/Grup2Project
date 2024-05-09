4bd56884aed37f3d3cd80436d5f5952488107384
Ivan Andrada <ivanandradavalenzuela@gmail.com>
2024-05-09 12:34:10 +0200

    Update Review Service

--- Backend/Spring/src/main/java/com/App/Grupo2/comtrollers/ReviewController.java
+++ Backend/Spring/src/main/java/com/App/Grupo2/comtrollers/ReviewController.java
@@ -1,14 +1,11 @@
 package com.App.Grupo2.comtrollers;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.App.Grupo2.models.Review;
-import com.App.Grupo2.repositories.ReviewRepository;
+import com.App.Grupo2.services.ReviewServices;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,48 +15,41 @@ import org.springframework.web.bind.annotation.PutMapping;
 @CrossOrigin(origins = "*")
 @RestController
 public class ReviewController {
-    ReviewRepository rr;
+    ReviewServices reviewServices;
 
-    ReviewController(ReviewRepository rr) {
-        this.rr = rr;
+    ReviewController(ReviewServices reviewServices) {
+        this.reviewServices = reviewServices;
     }
 
     @GetMapping("/reviews")
     public List<Review> getAll() {
-        return this.rr.findAll();
+        return reviewServices.getAll();
     }
 
     @GetMapping("/reviews/id/{id}")
     public Review oneById(@PathVariable("id") int id) {
-        Optional<Review> r = this.rr.findById(id);
-        if (r.isEmpty()) {
-            return null;
-        }
-        return r.get();
+        return reviewServices.oneById(id).orElse(null);
     }
 
     @PostMapping("/reviews")
-    public int addReviews(@RequestBody Review entity) {
-        Review review = this.rr.saveAndFlush(entity);
+    public int addReviews(@RequestBody Review newReview) {
+        Review review = reviewServices.addReview(newReview);
         return review.getId();
     }
 
     @DeleteMapping("/reviews/{id}")
     public String deleteReview(@PathVariable("id") int id) {
-        this.rr.deleteById(id);
+        reviewServices.deleteReview(id);
         return "Review eliminada";
     }
 
     @PutMapping("/reviews/{id}")
-    public String putReview(@PathVariable int id, @RequestBody Review entity) {
-        Review revTemp = this.oneById(id);
-        if (revTemp == null) {
-            return "No encontrado";
+    public String putReview(@PathVariable int id, @RequestBody Review reviewToUpdate) {
+        Review review = reviewServices.putReview(id, reviewToUpdate);
+        if (review != null) {
+            return "Review actualizada";
         }
-        revTemp.setDes(entity.getDes());
-        revTemp.setRatingReview(entity.getRatingReview());
-        this.rr.save(entity);
-        return "Review actualizada";
+        return "Review no encontrada";
     }
 
 }
