81b99aa000bfbcf3b1004674dcbcc17e868bade5
Rolkany <rolkanys@gmail.com>
2024-06-03 11:03:03 +0200

    services

---
+++ FrontEnd/vit@/react_App/src/services/EventContext.jsx
@@ -0,0 +1,17 @@
+import React, { createContext, useState, useContext } from "react";
+
+const EventContext = createContext();
+
+export const EventProvider = ({ children }) => {
+  const [events, setEvents] = useState([]);
+
+  return (
+    <EventContext.Provider value={{ events, setEvents }}>
+      {children}
+    </EventContext.Provider>
+  );
+};
+
+export const useEventContext = () => {
+  return useContext(EventContext);
+};
