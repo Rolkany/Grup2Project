451d41942131fbf509f884bec5474d27ea65ae58
Rolkany <rolkanys@gmail.com>
2024-06-03 10:54:00 +0200

    Event update

--- FrontEnd/vit@/react_App/src/components/Event.jsx
+++ FrontEnd/vit@/react_App/src/components/Event.jsx
@@ -1,47 +1,87 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+// EventPreview.js
+import React from "react";
+import "./Event.css";
 
-import eventsServices from '../services/eventServices';
-import userServices from '../services/userServices';
-import languageServices from '../services/languageServices';
+function Event({ event }) {
+  const {
+    id,
+    imgUrl,
+    title,
+    eventDate,
+    des,
+    type,
+    language,
+    location,
+    created_By,
+  } = event;
 
-const Event = () => {
-  const events = eventsServices();
-  const users = userServices();
-  const languages = languageServices();
+  // Función para formatear la fecha y hora
+  const formatDate = dateTimeString => {
+    const eventDateTime = new Date(dateTimeString);
+    const formattedDate = eventDateTime.toLocaleDateString("es-ES", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+    const formattedTime = eventDateTime.toLocaleTimeString("es-ES", {
+      hour: "numeric",
+      minute: "numeric",
+    });
+    return { formattedDate, formattedTime };
+  };
+
+  // Obtener la fecha y hora formateadas
+  const { formattedDate, formattedTime } = formatDate(eventDate);
 
   return (
     <>
-      {events.map((item) => {
-        return (
-          <Card style={{ width: '18rem' }} key={item.id}>
-            <Card.Img variant="top" src={item.imgUrl} />
-            <Card.Body>
-              <Card.Title>{item.title}</Card.Title>
-              <Card.Text>Próximo evento el {item.eventDate} !!!</Card.Text>
-              {users.map((element) => {
-                if (element.id === item.createdBy) {
-                  return <p key={element.id}>Creado por: {element.userName}</p>;
-                }
-              })}
-              {languages.map((language) => {
-                if (language.id === item.language_id) {
-                  return (
-                    <>
-                      <ul>
-                        <li key={language.id}> Idioma: {language.language}</li>
-                      </ul>
-                    </>
-                  );
-                }
-              })}
-              <Button variant="primary">Inscribete!</Button>
-            </Card.Body>
-          </Card>
-        );
-      })}
+      <main>
+        <div className="card">
+          <div
+            className="background"
+            style={{ backgroundImage: `url(${event.imgUrl})` }}
+          >
+            <div className="info">
+              <div>
+                <h1 className="title">{title}</h1>
+              </div>
+              <div className="infobox">
+                <div className="subTitle">
+                  <h4>
+                    {id} organizará un {type} en {location}.
+                  </h4>
+                </div>
+                <div className="container">
+                  <p className="description row">{des}</p>
+                </div>
+                <div className="dateTime">
+                  <p>Fecha</p>
+                  <br />
+                  <p>{formattedDate}</p>
+                  <p>Hora</p>
+                  <br />
+                  <p>{formattedTime}</p>
+                </div>
+                <div className="fliBox">
+                  <div></div>
+                  <div>
+                    <p>Lugar</p>
+                    <br />
+                    <p>{location}</p>
+                  </div>
+                  <div>
+                    <p>Idioma</p>
+                    <br />
+                    <p>{language}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   );
-};
+}
 
 export default Event;
