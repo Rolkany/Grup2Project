99c1bd2bcd8c5dbad2477a317ddd4b6cc05a48b8
Rolkany <rolkanys@gmail.com>
2024-06-03 10:59:39 +0200

    EvenPreviev update

---
+++ FrontEnd/vit@/react_App/src/components/EventPreview.jsx
@@ -0,0 +1,62 @@
+// EventPreview.js
+import React from "react";
+import "./EventPreview.css";
+
+const EventPreview = ({
+  eventPicture,
+  eventTitle,
+  eventDate,
+  eventType,
+  eventLanguage,
+  eventLocation,
+  eventDescription,
+}) => (
+  <div className="preview_box">
+    <div className="wraped">
+      <h2 className="ev_pw">Event Preview</h2>
+      {eventPicture && (
+        <div>
+          <img
+            className="img-preview"
+            src={URL.createObjectURL(eventPicture)}
+            alt="Preview"
+          />
+        </div>
+      )}
+    </div>
+    <div className="pr_title">
+      <p>
+        <strong>Event Title:</strong> {eventTitle}
+      </p>
+    </div>
+    <div>
+      <p>
+        <strong>Date:</strong> {eventDate}
+      </p>
+      <div>
+        <p>
+          <strong>Type:</strong> {eventType}
+        </p>
+      </div>
+      <div>
+        <p>
+          <strong>Language:</strong>{" "}
+          {eventLanguage.map(lang => lang.label).join(", ")}
+        </p>
+      </div>
+      <div>
+        <p>
+          <strong>Location:</strong>{" "}
+          {eventLocation.map(loc => loc.label).join(", ")}
+        </p>
+      </div>
+      <div>
+        <p>
+          <strong>Description:</strong> {eventDescription}
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
+export default EventPreview;
